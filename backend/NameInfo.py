8ea7893ce1bfd9414f57ad04c4c36fbb28faa766
leegilligana <leegilligana@carleton.edu>
2023-04-21 10:23:21 -0500

    removed NameInfo.py and NameQuery.py

--- backend/NameInfo.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class NameInfo:
-    name: str = "Jeff"
-    yearRangeStart: int = 2011
-    yearRangeEnd: int = 2019
-    gender: chr = 'X'
-    race: str = "All"
