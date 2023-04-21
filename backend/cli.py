8864603c17c340a7d45347309f483d7dbf758c3f
leegilligana <leegilligana@carleton.edu>
2023-04-21 10:18:55 -0500

    fixed command line bug, deleted redundant files

--- backend/cli.py
+++ backend/cli.py
@@ -12,7 +12,7 @@ def main():
             print(createNameQuery("Jeff", 2011, 2012, 'M', "White", 1, 1))
         elif (userInput.lower() == "help"):
             print("Enter 'create results' or 'create query' to test functionality. Enter 'exit' to exit.")
-        else:
+        elif (userInput.lower() != "exit"):
             print("Invalid command, enter 'help' for a list of commands or 'exit' to exit")
         
 
