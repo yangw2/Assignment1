b9f677ca56ed6f6613f7e7cc278ded4e6384c38c
CONORBON <babcockc@carleton.edu>
2023-04-24 10:30:59 -0500

    formatted data types in SQL for our columns of interest

--- sqlLab/createtable.sql
+++ sqlLab/createtable.sql
@@ -4,9 +4,7 @@ CREATE TABLE earthquakes (
   quaketime time with time zone,
   latitude real,
   longitude real,
-  quakedepth real,
   mag real,
   id text,
-  place text,
-  quaketype text
+  place text
 );
